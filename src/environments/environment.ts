d0105cc5bae593a033524bb2b9af4411f5b01fc1
lukesh kolhe <lukesh.kolhe321@gmail.com>
2020-10-07 20:50:29 +0530

    SRC updated

---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebaseConfig : {
+    apiKey: "AIzaSyAyZKOu5OtUG4W4DoI52B8gX2C1GKllu1k",
+    authDomain: "ksm-bookpicker.firebaseapp.com",
+    databaseURL: "https://ksm-bookpicker.firebaseio.com",
+    projectId: "ksm-bookpicker",
+    storageBucket: "ksm-bookpicker.appspot.com",
+    messagingSenderId: "712859235684",
+    appId: "1:712859235684:web:1798bf78369e2de1e2b671"
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
