689cbc81d645d4e4a912194f307f10a7042f5160
BugOP <surajbbd.in@gmail.com>
2020-10-10 19:04:26 +0530

    removed matSelcetModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { MatButtonModule} from '@angular/material/button'
-import { MatSelectModule } from '@angular/material/select';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSidenavModule} from '@angular/material/sidenav';
