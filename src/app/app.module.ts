3acfc22e3499ac3b3dfa69eddf7804bebdc4e534
unknown <snehalpb01@gmail.com>
2020-10-10 19:00:26 +0530

    removed reactive forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
+
 
 import { MatButtonModule} from '@angular/material/button'
 import { MatSelectModule } from '@angular/material/select';
